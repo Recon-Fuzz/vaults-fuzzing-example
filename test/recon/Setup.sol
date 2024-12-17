a1ff5efec5fa4433a94f946c0a4c9a26bb0afabc
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-12-17 15:11:07 -0300

    chore: compiling

--- test/recon/Setup.sol
+++ test/recon/Setup.sol
@@ -2,12 +2,9 @@
 pragma solidity ^0.8.0;
 
 import {BaseSetup} from "@chimera/BaseSetup.sol";
-import "src/Counter.sol";
 
 abstract contract Setup is BaseSetup {
-    Counter counter;
 
     function setup() internal virtual override {
-        counter = new Counter();
     }
 }

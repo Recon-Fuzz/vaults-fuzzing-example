d5feb9b4e948824595bfab6bc82418335d0807f4
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-06-30 17:34:55 -0400

    chore: clamping in handlers to make checks easier

--- test/recon/targets/VaultTargets.sol
+++ test/recon/targets/VaultTargets.sol
@@ -10,44 +10,41 @@ import {vm} from "@chimera/Hevm.sol";
 abstract contract VaultTargets is Properties, BaseTargetFunctions {
 
     function vault_approve(address spender, uint256 value) public asActor {
-        __before();
         vault.approve(spender, value);
-        __after();
-
-        eq(_before.pricePerShare, _after.pricePerShare, "pricePerShare should not change");
     }
     
-    function vault_transfer(address to, uint256 value) public asActor {
-        __before();
+    /// @dev We clamp the possible recipients for the property_total_supply_solvency check
+    function vault_transfer(uint256 toEntropy, uint256 value) public asActor {
+        address to = _getRandomActor(toEntropy);
         vault.transfer(to, value);
-        __after();
-
-        eq(_before.pricePerShare, _after.pricePerShare, "pricePerShare should not change");
     }
 
-    function vault_deposit(uint256 assets, address receiver) public updateGhosts asActor {        
-        vault.deposit(assets, receiver);
+    function vault_deposit(uint256 assets) public updateGhosts asActor {        
+        vault.deposit(assets, _getActor());
     }
 
-    function vault_mint(uint256 shares, address receiver) public updateGhosts asActor {
+    function vault_mint(uint256 shares) public updateGhosts asActor {
         uint256 expectedAssets = vault.previewMint(shares);
-        uint256 vaultBalanceBefore = underlyingAsset.balanceOf(address(this));
+        uint256 vaultBalanceBefore = underlyingAsset.balanceOf(address(vault));
         
-        vault.mint(shares, receiver);
+        vault.mint(shares, _getActor());
         
-        uint256 vaultBalanceAfter = underlyingAsset.balanceOf(address(this));
+        uint256 vaultBalanceAfter = underlyingAsset.balanceOf(address(vault));
         eq(vaultBalanceAfter, vaultBalanceBefore + expectedAssets, "vaultBalanceAfter must increase by expectedAssets amount");
     }
 
-    function vault_redeem(uint256 shares, address receiver, address owner) public asActor {
-        __before();
-        vault.redeem(shares, receiver, owner);
-        __after();
-
-        eq(_after.vaultTotalShares, _before.vaultTotalShares - shares, "vault totalSupply should decrease by shares amount");
+    function vault_redeem(uint256 shares) public updateGhosts asActor {
+        vault.redeem(shares, _getActor(), _getActor());
     }
 
-    function vault_withdraw(uint256 assets, address receiver, address owner) public updateGhosts asActor {
-        vault.withdraw(assets, receiver, owner);
+    function vault_withdraw(uint256 assets) public updateGhosts asActor {
+        vault.withdraw(assets, _getActor(), _getActor());
+    }
+
+    /// === Helpers === ///
+    function _getRandomActor(uint256 entropy) internal view returns (address) {
+        address[] memory actors = _getActors();
+        uint256 randomIndex = entropy % actors.length;
+        return actors[randomIndex];
     }
 }

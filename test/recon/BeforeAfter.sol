9fb4a1e20371a6bac7dc0a4f4297c2134a2781c4
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-07-08 14:08:23 -0400

    chore: implement better property

--- test/recon/BeforeAfter.sol
+++ test/recon/BeforeAfter.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.0;
 
 import {Setup} from "./Setup.sol";
 
+enum OpType {
+    DEFAULT,
+    ADD,
+    REMOVE
+}
+
 // ghost variables for tracking state variable values before and after function calls
 abstract contract BeforeAfter is Setup {
     struct Vars {
@@ -13,22 +19,29 @@ abstract contract BeforeAfter is Setup {
 
     Vars internal _before;
     Vars internal _after;
+    OpType internal currentOpType;
 
     modifier updateGhosts() {
+        currentOpType = OpType.DEFAULT;
         __before();
         _;
         __after();
     }
 
+    modifier updateGhostsWithOpType(OpType opType) {
+        currentOpType = opType;
+        __before();
+        _;
+        __after();
+    }   
+
     function __before() internal {
-        _before.vaultTotalShares = vault.totalSupply();
-        _before.ghostTotalShares = vault.balanceOf(address(this));
-        _before.pricePerShare = vault.previewMint(10**vault.decimals());
+        _before.pricePerShare = vault.convertToShares(10**vault.decimals());
+        // _before.pricePerShare = vault.previewMint(10**vault.decimals());
     }
 
     function __after() internal {
-        _after.vaultTotalShares = vault.totalSupply();
-        _after.ghostTotalShares = vault.balanceOf(address(this));
-        _after.pricePerShare = vault.previewMint(10**vault.decimals());
+        _after.pricePerShare = vault.convertToShares(10**vault.decimals());
+        // _after.pricePerShare = vault.previewMint(10**vault.decimals());
     }
 }
--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -5,28 +5,13 @@ import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {Asserts} from "@chimera/Asserts.sol";
 
-import {BeforeAfter} from "./BeforeAfter.sol";
+import {BeforeAfter, OpType} from "./BeforeAfter.sol";
 
 abstract contract Properties is BeforeAfter, Asserts {
-
-    // @dev Property: Total assets should always equal the sum of all depositors' claimable assets
-    // NOTE: This detects when precision loss allows attackers to claim more than they deposited
-    function property_totalAssetsEqualSumOfClaims() public {
-        uint256 totalClaimable = 0;
-
-        address[] memory actors = _getActors();
-        
-        // Sum up what each holder can claim based on their shares
-        for (uint256 i = 0; i < actors.length; i++) {
-            address actor = actors[i];
-            uint256 shares = vault.balanceOf(actor);
-            totalClaimable += vault.convertToAssets(shares);
+    function property_user_cannot_change_price_per_share() public {
+        if(currentOpType == OpType.ADD || currentOpType == OpType.REMOVE) {
+            eq(_before.pricePerShare, _after.pricePerShare, "price per share should not change with user operations");
         }
-        
-        uint256 actualAssets = vault.totalAssets();
-        
-        // if vault has more assets than the sum of all depositors' claimable assets, some depositors have lost their funds
-        lte(actualAssets, totalClaimable, "totalAssets should equal totalClaimable");
     }
 
 }

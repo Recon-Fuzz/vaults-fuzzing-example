1957e327ae5e76bbcf460757cc241be9821e96c1
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-07-08 14:32:06 -0400

    chore: readd removed before/after updates

--- test/recon/targets/VaultTargets.sol
+++ test/recon/targets/VaultTargets.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
-import {BeforeAfter} from "test/recon/BeforeAfter.sol";
+import {BeforeAfter, OpType} from "test/recon/BeforeAfter.sol";
 import {Properties} from "test/recon/Properties.sol";
 import {vm} from "@chimera/Hevm.sol";
 
@@ -17,11 +17,11 @@ abstract contract VaultTargets is BaseTargetFunctions, Properties {
         vault.transfer(to, value);
     }
 
-    function vault_deposit(uint256 assets) public updateGhosts asActor {        
+    function vault_deposit(uint256 assets) public updateGhostsWithOpType(OpType.ADD) asActor {        
         vault.deposit(assets, _getActor());
     }
 
-    function vault_mint(uint256 shares) public updateGhosts asActor {
+    function vault_mint(uint256 shares) public updateGhostsWithOpType(OpType.ADD) asActor {
         uint256 expectedAssets = vault.previewMint(shares);
         uint256 actorBalanceBefore = underlyingAsset.balanceOf(_getActor());
         
@@ -32,11 +32,11 @@ abstract contract VaultTargets is BaseTargetFunctions, Properties {
         // eq(actorBalanceAfter, actorBalanceBefore - expectedAssets, "actor balance should decrease by expectedAssets amount");
     }
 
-    function vault_redeem(uint256 shares) public updateGhosts asActor {
+    function vault_redeem(uint256 shares) public updateGhostsWithOpType(OpType.REMOVE) asActor {
         vault.redeem(shares, _getActor(), _getActor());
     }
 
-    function vault_withdraw(uint256 assets) public updateGhosts asActor {
+    function vault_withdraw(uint256 assets) public updateGhostsWithOpType(OpType.REMOVE) asActor {
         vault.withdraw(assets, _getActor(), _getActor());
     }
 }
